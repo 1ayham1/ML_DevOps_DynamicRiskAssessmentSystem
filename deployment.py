3693726b7f3d2f8db489d1e0398f0bca41820efa
me <learn.ayham@zoho.com>
2021-08-26 16:40:42 +0200

    copy data to deployment folder

--- deployment.py
+++ deployment.py
@@ -7,20 +7,39 @@ from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import json
+from shutil import copy
 
 
-
-##################Load config.json and correct path variable
+#Load config.json and correct path variable
 with open('config.json','r') as f:
     config = json.load(f) 
 
 dataset_csv_path = os.path.join(config['output_folder_path']) 
 prod_deployment_path = os.path.join(config['prod_deployment_path']) 
+model_folder = os.path.join(config['output_model_path'])
 
-
-####################function for deployment
+#function for deployment
 def store_model_into_pickle(model):
-    #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
+    """
+    copy the latest pickle file, the latestscore.txt value, 
+    and the ingestfiles.txt file into the deployment directory
+    """
+
+    model_path = os.path.join(model_folder,"trainedmodel.pkl")
+    copy(model_path, prod_deployment_path)
+
+    score_path = os.path.join(model_folder,"latestscore.txt")
+    copy(score_path, prod_deployment_path)
+
+    ingested_path = os.path.join(dataset_csv_path,'ingestedfiles.txt')
+    copy(ingested_path, prod_deployment_path)
+
+if __name__ == "__main__":
+    store_model_into_pickle()     
+    
+
+
+
         
         
         
