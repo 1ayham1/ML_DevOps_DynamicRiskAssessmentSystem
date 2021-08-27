6a6ba9dc7e8c913e3b219f54f3348f7264b8e44e
me <learn.ayham@zoho.com>
2021-08-27 17:27:38 +0200

    implementing and testing related api calls

--- apicalls.py
+++ apicalls.py
@@ -1,20 +1,28 @@
 import requests
+import os
+import json
 
 #Specify a URL that resolves to your workspace
 URL = "http://127.0.0.1/"
 
-
-
 #Call each API endpoint and store the responses
-response1 = #put an API call here
-response2 = #put an API call here
-response3 = #put an API call here
-response4 = #put an API call here
+call_1 = requests.post('http://127.0.0.1:8000/prediction?file_location=testdata/testdata.csv').text
+call_2 = requests.get('http://127.0.0.1:8000/scoring').text 
+call_3 = requests.get('http://127.0.0.1:8000/summarystats').text
+call_4 = requests.get('http://127.0.0.1:8000/diagnostics').text 
 
 #combine all API responses
-responses = #combine reponses here
+responses = call_1 + "\n" + call_2 + "\n" + call_3 + "\n" + call_4
 
 #write the responses to your workspace
 
+#Load config.json and get path variables
+with open('config.json','r') as f:
+    config = json.load(f) 
 
+model_folder = os.path.join(config['output_model_path'])
+output_file = os.path.join(model_folder,"apireturns.txt")
+
+with open(output_file, 'w') as f:
+   f.write(responses)
 
