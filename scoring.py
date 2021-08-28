a8d8ff8ad87b53ec9a909a567cdcf3cdcfb7f2df
me <learn.ayham@zoho.com>
2021-08-28 12:47:00 +0200

    restructuring and testing

--- scoring.py
+++ scoring.py
@@ -1,12 +1,18 @@
+"""Calculating F1-Score of a trained model"""
+
 import pickle
 import os
 import json
+import logging
 
 import pandas as pd
 from sklearn import metrics
 
+from training import data_load
 
 
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
 
 #Load config.json and get path variables
 with open('config.json','r') as f:
@@ -14,46 +20,58 @@ with open('config.json','r') as f:
 
 dataset_csv_path = os.path.join(config['output_folder_path']) 
 test_data_path = os.path.join(config['test_data_path']) 
-#model_folder = os.path.join(config['output_model_path']) 
 model_folder = os.path.join(config['prod_deployment_path']) 
 
-#Function for model scoring
+
 def score_model(new_data = False):
-    """
-    this function takes a trained model, 
-    load test data, and calculate an F1 score for the model relative 
-    to the test data. it writes the result to the latestscore.txt file
+    """Function for model scoring
+
+    Given a trained model, relevent test data is loaded, and F1 score is
+    then calculated. Result is written back to appropriate folder with 
+    the name [latestscore.txt] 
+
+    new_data : if new data is available, calculate the score on 
+                related training data
     """
 
-    #load the model
+    
+    logger.info("read and load trained model")
+
     model_path = os.path.join(model_folder,"trainedmodel.pkl")
     with open(model_path, 'rb') as file:
         model = pickle.load(file)
     
-    #read data, if we are ingesting new data, then new data is used for scoring
+    #read data, if new data is ingested, then new data is used for scoring
     if(new_data):
         
-        file_name = os.path.join(dataset_csv_path,"finaldata.csv")
+        logger.info("Scoring data_train: new data is ingested.")
+
+        file_name = "data_train.csv"
+        _, X_features, y_response = data_load(dataset_csv_path, file_name)
     
     else:
-        file_name = os.path.join(test_data_path,"testdata.csv")
+        logger.info("Scoring data_test")
+
+        file_name = "data_test.csv"
+        _, X_features, y_response = data_load(test_data_path, file_name)
     
-    df = pd.read_csv(file_name,low_memory=False)
 
-    X = df.loc[:,['lastmonth_activity','lastyear_activity','number_of_employees']].values.reshape(-1, 3)
-    y = df['exited'].values.reshape(-1, 1).ravel()
+    logger.info("Model prediction and calculating F1 score")
 
-    predicted = model.predict(X)
+    predicted = model.predict(X_features)
+    f1score = metrics.f1_score(predicted,y_response)
 
-    f1score = metrics.f1_score(predicted,y)
+    logger.info("Saving F1 score to a file")
 
-    #writing score
     score_path = os.path.join(model_folder,"latestscore.txt")
     with open(score_path, 'w') as f:
         f.write(str(f1score))
     
     return f1score
 
-
+if __name__ == "__main__":
+    f1score = score_model()
+    logger.info(f"\nwelldone f1_score is: {f1score}...\n"
+                f"check relevent folders for output")
 
 
