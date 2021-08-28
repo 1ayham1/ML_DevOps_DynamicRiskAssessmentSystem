3f194e86bc9fbb8b3c7f6ab6babecd4a66a0c5bc
me <learn.ayham@zoho.com>
2021-08-28 21:19:15 +0200

    fixing call to prediction, adding log, and adjusting other methods to match the changes in diagonostics.py

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-from flask import Flask, session, jsonify, request
-import pandas as pd
-import numpy as np
-import pickle
-
+"""Flask Endpoints to train/track ML pipline"""
 import json
 import os
+import logging
+import pandas as pd
 
-from diagnostics import model_predictions,dataframe_summary,execution_time,missing_data,outdated_packages_list
-from scoring import score_model
+from flask import Flask, session, jsonify, request
 
-#Set up variables for use in our script
+import diagnostics 
+import scoring
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
 app = Flask(__name__)
 app.secret_key = '1652d576-484a-49fd-913a-6879acfa6ba4'
 
@@ -21,35 +25,35 @@ model_folder = os.path.join(config['output_model_path'])
 
 prediction_model = None
 
-#Prediction Endpoint
+logger.info("Prediction Endpoint")
+
 @app.route("/prediction", methods=['POST','OPTIONS'])
 def predict():        
     """ 
     take a dataset's file location as its input, 
     returns value for prediction outputs
     """
-    #file_location = request.args.get('file_location')
-    #filename = os.path.join(file_location,"testdata.csv")
-
-    #or pass file name directly using fullpath .../..../x.csv
-    filename = request.args.get('file_location')
+    input_file = request.args.get('file_location')
     
-    data_df = pd.read_csv(filename)
-    predicted = model_predictions(data_df)
+    folder = os.path.dirname(input_file)
+    filename = os.path.basename(input_file)
+
+    predicted = diagnostics.model_predictions(folder,filename)
 
     return str(predicted)
 
+logger.info("Scoring Endpoint")
 
-#Scoring Endpoint
 @app.route("/scoring", methods=['GET','OPTIONS'])
 def score_stats():        
     """check the score of the deployed model, returns F1 score"""
     
-    f1score = score_model()
-    return str(f1score)
+    f1_score_ = scoring.score_model()
+    return str(f1_score_)
 
 
-#Summary Statistics Endpoint
+logger.info("Summary Statistics Endpoint")
+
 @app.route("/summarystats", methods=['GET','OPTIONS'])
 def summary_stats():        
     """
@@ -57,12 +61,12 @@ def summary_stats():
     return a list of all calculated summary statistics
     """
     
-    summary_list = dataframe_summary()
+    summary_list = diagnostics.dataframe_summary()
 
     return str(summary_list)
 
+logger.info("Diagnostics Endpoint")
 
-#Diagnostics Endpoint
 @app.route("/diagnostics", methods=['GET','OPTIONS'])
 def diag_stats():        
     """
@@ -70,17 +74,15 @@ def diag_stats():
     percent NA values
     and list of outdated_packages
     """
-    p_missing = missing_data()
-    ingest_time, train_time = execution_time()
-    outdated = outdated_packages_list()
+    p_missing = diagnostics.missing_data()
+    ingest_time, train_time = diagnostics.execution_time()
+    outdated_files = diagnostics.outdated_packages_list()
 
     all_output = "Missing Data:\n" +p_missing +"\nTime:\nIngestion: " + str(ingest_time) + \
-                "\tTrain: "+ str(train_time) + "\nOutdated Packages:\n" + outdated
+                "\tTrain: "+ str(train_time) + "\nOutdated Packages:\n" + outdated_files
 
     return all_output
     
-
-
 
 if __name__ == "__main__":    
     app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
