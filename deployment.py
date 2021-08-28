50fc6a46f10ca78fb9841a08c798c5e3c6e05d18
me <learn.ayham@zoho.com>
2021-08-28 14:20:57 +0200

    writing a more modular function to move files

--- deployment.py
+++ deployment.py
@@ -1,14 +1,15 @@
-from flask import Flask, session, jsonify, request
-import pandas as pd
-import numpy as np
-import pickle
-import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-import json
-from shutil import copy
+"""Trasfering verified output to Deployment folder"""
 
+
+import os
+import json
+import logging
+
+from shutil import copy2
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
 
 #Load config.json and correct path variable
 with open('config.json','r') as f:
@@ -18,24 +19,29 @@ dataset_csv_path = os.path.join(config['output_folder_path'])
 prod_deployment_path = os.path.join(config['prod_deployment_path']) 
 model_folder = os.path.join(config['output_model_path'])
 
-#function for deployment
-def store_model_into_pickle(model):
-    """
+
+def store_model_into_pickle():
+    """copy latest files and metadata to deployment folder
+
     copy the latest pickle file, the latestscore.txt value, 
     and the ingestfiles.txt file into the deployment directory
     """
 
-    model_path = os.path.join(model_folder,"trainedmodel.pkl")
-    copy(model_path, prod_deployment_path)
+    names_and_paths ={
+        "trainedmodel.pkl":model_folder,
+        "latestscore.txt":model_folder,
+        'ingestedfiles.txt':dataset_csv_path
+        }
 
-    score_path = os.path.join(model_folder,"latestscore.txt")
-    copy(score_path, prod_deployment_path)
+    for name, folder in names_and_paths.items():
+        
+        copy2(os.path.join(folder,name), prod_deployment_path)
 
-    ingested_path = os.path.join(dataset_csv_path,'ingestedfiles.txt')
-    copy(ingested_path, prod_deployment_path)
+    
 
 if __name__ == "__main__":
-    store_model_into_pickle()     
+    store_model_into_pickle()  
+    logger.info("welldone ...check relevent folders for output\n")   
     
 
 
